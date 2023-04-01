7adf08464ac37abc67d7afbfcdf5f32cf1fdb1ee
George Bryan Clemons <gbc466@gmail.com>
2023-03-31 21:40:03 -0500

    finished signup & signin pages and  functionality. Began HomePage

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 const app = express();
 
@@ -20,6 +24,10 @@ const userSchema = mongoose.Schema({
                 type: String,
                 required: true
         },
+        userName: {
+                type: String,
+                required: true
+        },
         sex: {
                 type: String,
                 required: true
@@ -35,45 +43,78 @@ const userSchema = mongoose.Schema({
         height: {
                 type: Number,
                 required: true
+        },
+        userPassword: {
+                type: String,
+                required: true
         }
 
 })      
 
 const user = mongoose.model("user", userSchema);
 
-// const BryanC = new user({
-//         name: "Bryan Clemons",
-//         age: 29,
-//         weight: 180,
-//         height: 67,
-//         sex: "male"
-// })
-// BryanC.save();
-
-app.get("/", function (req, res) {
+app.get("/signup", function (req, res) {
         res.render('signup');
 });
+
+app.get("/signin", function(req, res){
+        res.render('signin');
+})
+
+app.get("/home", function(req, res){
+        res.render('home');
+})
 
 app.post("/newUser", function(req, res){
         const firstName = req.body.firstName;
         const lastName = req.body.lastName;
+        const userName = req.body.userName;
         const sex = req.body.sex;
         const age = req.body.age;
         const weight = req.body.weight;
         const feet = Number(req.body.feet);
         const inches = Number(req.body.inches);
         const height = Number((feet * 12) + inches);
-        console.log(firstName, lastName, sex, age, weight, feet, inches, height);
-
-        const newUser = new user({
-                firstName: firstName,
-                lastName: lastName,
-                sex: sex,
-                age: age,
-                weight: weight,
-                height: height
+        const userPassword = req.body.password;
+        bcrypt.hash(userPassword, saltRounds, function(err, hash){
+                if(err){
+                        console.log(err);
+                } else {
+                        const hashedPassword = hash;
+                        const newUser = new user({
+                                firstName: firstName,
+                                lastName: lastName,
+                                userName: userName,
+                                sex: sex,
+                                age: age,
+                                weight: weight,
+                                height: height,
+                                userPassword: hashedPassword
+                        })
+                        newUser.save();
+                        res.render("signin");
+                }
         })
-        newUser.save();
+});
+
+app.post("/signin", function(req, res){
+        const userName = req.body.userName;
+        const password = req.body.password;
+        user.findOne({userName: userName})
+        .then(foundUser => {
+                const foundPassword = foundUser.userPassword;
+                console.log(foundPassword)
+                bcrypt.compare(password, foundPassword, function(err, result){
+                        if(err){
+                                console.log(err)
+                        } else if (result){
+                                console.log('Success')
+                        } else {
+                                console.log('incorrect password')
+                        }
+                })
+
+        })
 })
 
 
